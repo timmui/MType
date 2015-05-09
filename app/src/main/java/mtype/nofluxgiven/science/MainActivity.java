0d9280d1173c9294fb6c4719a583c39b4129a363
Jhony Guan <jhony.guan@gmail.com>
2015-05-09 02:15:21 -0400

    Added Azure REST Service
    Set up the Send Message functionality
    
    and...
    
    Do you want to build a snowmaaaannnn.

--- app/src/main/java/mtype/nofluxgiven/science/MainActivity.java
+++ app/src/main/java/mtype/nofluxgiven/science/MainActivity.java
@@ -8,16 +8,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+
+import java.net.MalformedURLException;
 
 
 public class MainActivity extends ActionBarActivity {
     private static String message = "";
     private static TextView displayMessage;
+    private static ServerHandler server;
+    //private MobileServiceClient mClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout layout = (LinearLayout)findViewById(R.id.layout);
+
+        server = new ServerHandler(this);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
         displayMessage = (TextView)findViewById(R.id.displayMessage);
         layout.setOrientation(LinearLayout.VERTICAL);
         Button dot = (Button)findViewById(R.id.dot);
@@ -44,7 +54,13 @@ public class MainActivity extends ActionBarActivity {
                 updateMessage();
             }
         });
-
+        Button sendButton = (Button) findViewById(R.id.sendMessage);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                server.SendMessage(message, "JJMO", "Pragash");
+            }
+        });
 
     }
     private static void updateMessage()
---
+++ app/src/main/java/mtype/nofluxgiven/science/ServerHandler.java
@@ -0,0 +1,52 @@
+package mtype.nofluxgiven.science;
+
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+
+import java.net.MalformedURLException;
+
+/**
+ * Created by Jhony Guan on 5/9/2015.
+ */
+public class ServerHandler {
+    private MobileServiceClient mClient;
+
+    public ServerHandler (MainActivity a){
+        // REST Client
+        try {
+            mClient = new MobileServiceClient(
+                    "https://ecetitanic-mtype.azure-mobile.net/",
+                    "YTuapDdxHvjrxaoUaQqVcHrduOibvW98",
+                    a
+            );
+        }
+        catch (MalformedURLException e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void SendMessage(String text, String sender, String receiver) {
+        Item item = new Item();
+        item.Text = text;
+        item.Receiver = sender;
+        item.Sender = receiver;
+        if(item.Text != null) {
+            mClient.getTable(Item.class).insert(item, new TableOperationCallback<Item>() {
+                public void onCompleted(Item entity, Exception exception, ServiceFilterResponse response) {
+                    if (exception == null) {
+                        // Insert succeeded
+
+                    } else {
+                        // Insert failed
+                    }
+                }
+            });
+        }
+    }
+
+    public void ReceiveMessages() {
+
+    }
+}
