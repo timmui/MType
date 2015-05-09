d6d6d5bf9199695053b39e0853bc633b9aa023f9
PragashSiva <pragash.acci@gmail.com>
2015-05-09 00:23:07 -0400

    functional morse code translator

--- app/src/main/java/mtype/nofluxgiven/science/MainActivity.java
+++ app/src/main/java/mtype/nofluxgiven/science/MainActivity.java
@@ -9,10 +9,15 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 
 public class MainActivity extends ActionBarActivity {
     private static String message = "";
     private static TextView displayMessage;
+    private static long prev = System.currentTimeMillis();
+    private static long cur = System.currentTimeMillis();
+    private static String decrypted = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +29,33 @@ public class MainActivity extends ActionBarActivity {
         dot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                prev = cur;
+                cur = System.currentTimeMillis();
+                if ((cur - prev) > 1000)
+                    message = message + "    ";
+                else if ((cur - prev) > 500)
+                    message = message + ' ';
+
                 message = message + ".";
-                updateMessage();
+                updateMessage(message);
             }
         });
         Button dash = (Button)findViewById(R.id.dash);
         dash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                prev = cur;
+                cur = System.currentTimeMillis();
+                if ((cur - prev) > 1000)
+                    message = message + "    ";
+                else if ((cur - prev) > 500)
+                    message = message +' ';
+
                 message = message + "-";
-                updateMessage();
+                updateMessage(message);
+
             }
         });
         Button space = (Button)findViewById(R.id.space);
@@ -41,13 +63,22 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v){
                 message = message + " ";
-                updateMessage();
+                updateMessage(message);
+            }
+        });
+        Button getMessage = (Button)findViewById(R.id.getMessage);
+        getMessage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                decrypted = hash(message);
+                updateMessage(decrypted);
+                message = "";
             }
         });
 
-
     }
-    private static void updateMessage()
+    private static void updateMessage(String message)
     {
         displayMessage.setText(message);
     }
@@ -72,5 +103,68 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public static String hash(String morseCode){
+        String message = "";
+
+        HashMap <String,Character> alphabet = new HashMap <> ();
+
+        //Letters
+        alphabet.put(".-",'a');
+        alphabet.put("-...",'b');
+        alphabet.put("-.-.",'c');
+        alphabet.put("-..",'d');
+        alphabet.put(".",'e');
+        alphabet.put("..-.",'f');
+        alphabet.put("--.",'g');
+        alphabet.put("....",'h');
+        alphabet.put("..",'i');
+        alphabet.put(".---",'j');
+        alphabet.put("-.-",'k');
+        alphabet.put(".-..",'l');
+        alphabet.put("--",'m');
+        alphabet.put("-.",'n');
+        alphabet.put("---",'o');
+        alphabet.put(".--.",'p');
+        alphabet.put("--.-",'q');
+        alphabet.put(".-.",'r');
+        alphabet.put("...",'s');
+        alphabet.put("-",'t');
+        alphabet.put("..-",'u');
+        alphabet.put("...-",'v');
+        alphabet.put(".--",'w');
+        alphabet.put("-..-",'x');
+        alphabet.put("-.--",'y');
+        alphabet.put("--..",'z');
+
+        //Numbers
+        alphabet.put("-----",'0');
+        alphabet.put(".----",'1');
+        alphabet.put("..---",'2');
+        alphabet.put("...--",'3');
+        alphabet.put("....-",'4');
+        alphabet.put(".....",'5');
+        alphabet.put("-....",'6');
+        alphabet.put("--...",'7');
+        alphabet.put("---..",'8');
+        alphabet.put("---.",'9');
+
+
+        String [] words = morseCode.split("    ");
+        String [] letters;
+        for (String word : words) {
+
+                letters = word.split(" ");
+                for (String letter : letters) {
+                    if (alphabet.get(letter) != null)
+                    message = message + alphabet.get(letter);
+                }
+                message = message + " ";
+
+        }
+
+        return message;
     }
 }
